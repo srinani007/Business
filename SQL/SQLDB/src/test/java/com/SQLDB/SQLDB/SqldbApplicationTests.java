9e49eb9d9d5c433a45db249566223b9cfcdf32fc
PRASANTH KUNCHANAPALLI <108681276+srinani007@users.noreply.github.com>
2024-10-06 17:53:41 -0400

    NewBASE

---
+++ SQL/SQLDB/src/test/java/com/SQLDB/SQLDB/SqldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SQLDB.SQLDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
