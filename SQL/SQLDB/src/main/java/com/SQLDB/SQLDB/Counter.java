65f3bebf50fca946b7467f3fe3a99a72d97dd544
srinani007 <kvsprasanth007@gmail.com>
2024-10-06 21:02:49 -0400

    Counter

--- SQL/SQLDB/src/main/java/com/SQLDB/SQLDB/Counter.java
+++ SQL/SQLDB/src/main/java/com/SQLDB/SQLDB/Counter.java
@@ -4,7 +4,11 @@ package com.SQLDB.SQLDB;
 public class Counter {
     private final String name;
     private int count = 0;
-    private int count1 = 0;
+    private static int count1 = 0;
+    private String Name = "Prasanth";
+    private String  Name2 = "Srividya";
+
+    
 
     public Counter(String id) {
         this.name = id;
@@ -21,6 +25,9 @@ public class Counter {
     @Override
     public String toString() {
         return name + ": " + count;
+    }
+    public static void main(String[] args) {
+        System.out.println("love: "+count1+ "marriage");
     }
 }
 
